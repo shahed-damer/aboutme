5c6bb88eb87569f6f9c4760c51df38b239994227
shahed qerbaa <shaheddamer@gmail.com>
2021-06-06 22:13:26 +0300

    aboutme

---
+++ js/app.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+let userName = prompt('whats your name');
+      console.log(userName);
+alert (`welcome ${userName}, to our page`);
+
+let userage = prompt('whats your age');
+      console.log(userage);
+alert('May God Give you long happy life');
+
+let userfav = prompt('Whats your favorite anime');
+      console.log(userfav);
+ alert('Great its an amazing anime') ;
+ 
+ let usergame = prompt('whats your favorite vedio game');
+      console.log(usergame);
+alert('Nice Choice');
+
+
